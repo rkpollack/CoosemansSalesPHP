47a7771f4cdbf015137bb76a5a595023c945d7b8
Bob Pollack <rkpollack@gmail.com>
2016-12-22 01:06:57 +0000

    changed to hello-world

--- web/index.php
+++ web/index.php
@@ -1,25 +1,11 @@
+<html><body>
 <?php
+// A simple web site in Cloud9 that runs through Apache
+// Press the 'Run' button on the top to start the web server,
+// then click the URL that is emitted to the Output tab of the console
 
-require('../vendor/autoload.php');
+echo 'Hello world from Cloud9!';
 
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
-
-$app->run();
+?>
+</body>
+</html>
