8a9c30bdc784a41724a045d8dcb9411a05928626
Bob Pollack <rkpollack@gmail.com>
2016-12-22 02:01:14 +0000

    this is it

--- web/index.php
+++ web/index.php
@@ -1,7 +1,17 @@
+<html><body>
 <?php
-
-$myarr = array('a' => "this is an associative array", 'b' => "this is too");
-
-print json_encode($myarr);
+$ort = $_GET['ort'];
+$url = "http://1webblvd.com/shpsls.htm";
+$offset = 3;
+if (strpos($ort,"arket") >0) $url = "http://dvr.coosemansla.com:81/stats/mktsls.htm";
+if (strpos($ort,"arket") >0) $offset = 0;
+$string = file_get_contents($url);
+$datestr = substr($string,121+$offset,2)."/".substr($string,124+$offset,2);
+$timestr = substr($string,163+$offset,5);
+$x3 = strpos($string,"Total All Items")+121;
+$amt = substr($string,$x3,12);
+echo ("$ort sales for $datestr at $timestr is $$amt");
 
 ?>
+</body>
+</html>
