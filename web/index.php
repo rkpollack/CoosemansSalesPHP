a685dfe0a5deb0d22148afcb5da9eadc7cd7b315
Bob Pollack <rkpollack@gmail.com>
2016-12-22 01:14:53 +0000

    json test

--- web/index.php
+++ web/index.php
@@ -1,11 +1,7 @@
-<html><body>
 <?php
-// A simple web site in Cloud9 that runs through Apache
-// Press the 'Run' button on the top to start the web server,
-// then click the URL that is emitted to the Output tab of the console
 
-echo 'Hello world from Cloud9!';
+$myarr = array('a' => "this is an associative array");
+
+print json_encode(myarr);
 
 ?>
-</body>
-</html>
